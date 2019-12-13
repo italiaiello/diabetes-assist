3b7617c1da93883383addd190e3e2335d31b1cd9
Andrew Aiello <andrew@Andrews-MBP.modem>
2019-12-13 19:45:51 +1100

    pushing the rest of the files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import ReactCalendar from './components/Calendar/calendar';
+
+function App() {
+  return (
+    <div className="App">
+      <ReactCalendar />
+    </div>
+  );
+}
+
+export default App;
