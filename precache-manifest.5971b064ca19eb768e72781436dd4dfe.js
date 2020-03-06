a51ebdc4696c8d4fe5e06492defed3ab143426a0
Andrew Aiello <andrew@Andrews-MBP.modem>
2020-03-06 21:35:56 +1100

    Updates

---
+++ precache-manifest.5971b064ca19eb768e72781436dd4dfe.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec310972493fc8e41a7dd72a9430f263",
+    "url": "/diabetes-assist/index.html"
+  },
+  {
+    "revision": "78accabdbbe84515e8e4",
+    "url": "/diabetes-assist/static/css/2.e9e7e959.chunk.css"
+  },
+  {
+    "revision": "b9c34d2f82ecd03bbaab",
+    "url": "/diabetes-assist/static/css/main.cb7d10b6.chunk.css"
+  },
+  {
+    "revision": "78accabdbbe84515e8e4",
+    "url": "/diabetes-assist/static/js/2.e8412e93.chunk.js"
+  },
+  {
+    "revision": "14cfafeab58f243640d9d112b6ecad16",
+    "url": "/diabetes-assist/static/js/2.e8412e93.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b9c34d2f82ecd03bbaab",
+    "url": "/diabetes-assist/static/js/main.a9b1aadf.chunk.js"
+  },
+  {
+    "revision": "dab8715f803afe6e4a9e",
+    "url": "/diabetes-assist/static/js/runtime-main.7738ec58.js"
+  },
+  {
+    "revision": "0aa811e098f282bb2e04005db63b7d96",
+    "url": "/diabetes-assist/static/media/AliceManini.0aa811e0.png"
+  },
+  {
+    "revision": "cf2826bee1457384e094c1af252f0b40",
+    "url": "/diabetes-assist/static/media/CamilleGreen.cf2826be.png"
+  },
+  {
+    "revision": "cb949d8246369b42ca98c7e4ded38534",
+    "url": "/diabetes-assist/static/media/Cybel.cb949d82.svg"
+  },
+  {
+    "revision": "c44f9a1608683c748a3d9836c2912a2b",
+    "url": "/diabetes-assist/static/media/DA_logo.c44f9a16.svg"
+  },
+  {
+    "revision": "79b7b69236ff34d3b0df5d78f194ddda",
+    "url": "/diabetes-assist/static/media/DanielleBrown.79b7b692.png"
+  },
+  {
+    "revision": "b204bfb2e73dc55b58578954645c2311",
+    "url": "/diabetes-assist/static/media/Emergency.b204bfb2.svg"
+  },
+  {
+    "revision": "7a94b1fc1ffa2c26f29ca4b13ed85dc4",
+    "url": "/diabetes-assist/static/media/Error.7a94b1fc.svg"
+  },
+  {
+    "revision": "772204fb547370e46fbe1dc3d5767a84",
+    "url": "/diabetes-assist/static/media/Health.772204fb.svg"
+  },
+  {
+    "revision": "5d1d9ad44e27b83522e15c6989cad452",
+    "url": "/diabetes-assist/static/media/Home.5d1d9ad4.svg"
+  },
+  {
+    "revision": "288634de2ba499e056c91f8f62ff84e9",
+    "url": "/diabetes-assist/static/media/Person.288634de.png"
+  },
+  {
+    "revision": "1cd073d377a489074afe1308ea2ec1ed",
+    "url": "/diabetes-assist/static/media/SquiresBrown.1cd073d3.png"
+  },
+  {
+    "revision": "aab869a40db571646453cc9433f4e9c9",
+    "url": "/diabetes-assist/static/media/Tick.aab869a4.svg"
+  },
+  {
+    "revision": "e91e0f551f7d65cc181a3b6f46947aa4",
+    "url": "/diabetes-assist/static/media/appointment.e91e0f55.svg"
+  },
+  {
+    "revision": "31e411e71dd27d51a9e1d228f17041bf",
+    "url": "/diabetes-assist/static/media/bgl-graph.31e411e7.png"
+  },
+  {
+    "revision": "2c9bc7d4f0b9ff02d99b07e71bfc40a8",
+    "url": "/diabetes-assist/static/media/calendar.2c9bc7d4.svg"
+  },
+  {
+    "revision": "50182865528f91efe172a74b7503c86d",
+    "url": "/diabetes-assist/static/media/meal.50182865.svg"
+  },
+  {
+    "revision": "15406dde2dc9ab0428c8edb2a1165e2d",
+    "url": "/diabetes-assist/static/media/microphone.15406dde.svg"
+  },
+  {
+    "revision": "f2d39dc2f418c4d7b5e9f75c7c83ca44",
+    "url": "/diabetes-assist/static/media/notifications.f2d39dc2.svg"
+  },
+  {
+    "revision": "1a006bfa8ebf9aa3364d924b4cb69963",
+    "url": "/diabetes-assist/static/media/plus_sign.1a006bfa.svg"
+  },
+  {
+    "revision": "06a549552a83c80bfece8e7047848275",
+    "url": "/diabetes-assist/static/media/ringing_phone.06a54955.svg"
+  },
+  {
+    "revision": "c263c8f131744f2de9584b5680f0fcc8",
+    "url": "/diabetes-assist/static/media/technician.c263c8f1.svg"
+  }
+]);
